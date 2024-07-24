cd91c687cd78b5056130b034df1cc5414aea3283
HOME <ljun88@naver.com>
2024-07-24 20:57:46 +0900

    시작

---
+++ src/main/java/com/example/prick/PrickApplication.java
@@ -0,0 +1,13 @@
+package com.example.prick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrickApplication.class, args);
+    }
+
+}
