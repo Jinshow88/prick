68209f3b1438adaa102cb23610ae0ccf490e12c2
HOME <ljun88@naver.com>
2024-07-24 21:22:44 +0900

    엔티티 만드는중

---
+++ src/main/java/com/example/prick/entity/User.java
@@ -0,0 +1,35 @@
+package com.example.prick.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+@Setter
+@Getter
+@Entity
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+
+//    @ColumnDefault("4")
+//    @Column(nullable = false)
+//    private SignInProviderType providerType;
+
+    @Column(length = 50, nullable = false)
+    private String uid;
+
+    @Column(length = 100, nullable = false)
+    private String upw;
+
+    @Column(length = 50, nullable = false)
+    private String nm;
+
+    @Column(length = 200)
+    private String pic;
+
+
+}
